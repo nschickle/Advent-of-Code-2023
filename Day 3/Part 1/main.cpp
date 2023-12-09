a0f6d32a4f369994ca4d629090e03455a238ee25
nschickle <nicholasschickle@gmail.com>
2023-12-08 20:45:02 -0500

    Tiny bit of work to get input into character array

--- Day 3/Part 1/main.cpp
+++ Day 3/Part 1/main.cpp
@@ -8,13 +8,18 @@ int main()
 {
     ifstream inputFile("../input.txt");
     string line;
-
+    int lineNum = 0;
+    char arr [140][140];
 
     if (inputFile.is_open())
     {
         while (getline(inputFile, line))
         {
-            cout << line << endl;
+            for (size_t i = 0; i < line.size(); i++)
+            {
+                arr[lineNum][i] = line[i];
+            }
+            lineNum++;
         }
     }
     inputFile.close();
