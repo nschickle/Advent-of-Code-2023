5b5bca08b847393d68df7937b957957d17b1c60f
nschickle <nicholasschickle@gmail.com>
2023-12-09 21:41:25 -0500

    Extracted every number and it's coordinates. Unsure if next step should be to search each area around the numbers or regex the symbols and do math to decide whether it counts.

--- Day 3/Part 1/main.cpp
+++ Day 3/Part 1/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 #include <fstream>
+#include <array>
+#include <regex>
 
 using namespace std;
 
@@ -8,19 +11,30 @@ int main()
 {
     ifstream inputFile("../input.txt");
     string line;
+    string tempStr;
+    regex potNum("\\d+");
+    smatch m;
     int lineNum = 0;
-    char arr [140][140];
+    string arr [140];
 
     if (inputFile.is_open())
     {
         while (getline(inputFile, line))
         {
-            for (size_t i = 0; i < line.size(); i++)
-            {
-                arr[lineNum][i] = line[i];
-            }
+            arr[lineNum] = line;
             lineNum++;
         }
+        for (size_t i = 0; i < arr->size(); i++)
+        {
+            tempStr = arr[i];
+            while(regex_search(tempStr, m, potNum)) 
+            {
+                cout << m.str() << " at " << m.position() << ", " << i << endl;
+                tempStr = m.suffix();
+            }
+        }
+        
+        
     }
     inputFile.close();
 }
